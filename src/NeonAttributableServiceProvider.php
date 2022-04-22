fc082dc0f076f9d10ab1de3351d918d2527800f8
Elementary Interactive\Balázs Ercsey <laze@laze.hu>
2022-04-22 09:59:06 +0200

    Database migrations and service provider.
    The very basics of the package.

---
+++ src/NeonAttributableServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Neon\Attributables;
+
+use Illuminate\Support\ServiceProvider;
+use \Illuminate\Contracts\Http\Kernel;
+
+class NeonAttributableServiceProvider extends ServiceProvider
+{
+
+  /** Bootstrap any application services.
+   *
+   * @param \Illuminate\Contracts\Http\Kernel  $kernel
+   *
+   * @return void
+   */
+  public function boot(Kernel $kernel): void
+  {
+    if ($this->app->runningInConsole()) {
+      
+      /** Export migrations.
+       */
+      $this->publishes([
+        __DIR__ . '/../database/migrations/create_attributes_table.php.stub'        => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_attributes_table.php'),
+        __DIR__ . '/../database/migrations/create_attribute_values_table.php.stub'  => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_attribute_values_table.php'),
+      ], 'migrations');
+    }
+  }
+}
